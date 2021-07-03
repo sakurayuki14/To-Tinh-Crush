43d49a391e2f65e5650224ad6b15a6e866924fcf
sakurayuki14 <86834408+sakurayuki14@users.noreply.github.com>
2021-07-03 15:34:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Hỏi một câu nhá!',
+    introDesc: `bắt đầu`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    title: 'Có yêu mừn nhìu khum =.= 😙',
+    desc: 'nhìu hay khum trả lời đê ',
+    btnYes: 'nhiều lắm lun<33',
+    btnNo: 'Không, có 1 tý :3',
+    question:'Tại sao lại thích mừn?',
+    btnReply: 'Gửi cho ai đó <3',
+    reply: 'Vì mừn vừa cao vừa beautiful <33333333',
+    mess: 'ahihi ngại quá 🥰. Yêu ghê 😘😘',
+    messDesc: 'cho đấm 1 cái nhe.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
